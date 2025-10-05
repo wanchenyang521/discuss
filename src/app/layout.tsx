13ab1b02554e5aa3d1d56a413eddce2e97734f33
joj <wan1183254286@icloud.com>
2025-10-05 20:45:39 +0800

    公共组件

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import {Providers} from "./providers";
+import Header from "@/component/header";
+import {Button} from "@heroui/react";
 const geistSans = Geist({
   variable: "--font-geist-sans",
   subsets: ["latin"],
@@ -28,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
       <Providers>
+          <Header />
           {children}
       </Providers>
       </body>
---
+++ src/component/header.tsx
@@ -0,0 +1,52 @@
+import {Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Button, Avatar} from "@heroui/react";
+
+import React from 'react';
+import {Input} from "@heroui/input";
+import {auth, signOut} from "@/auth";
+
+
+export const AcmeLogo = () => {
+    return (
+        <svg fill="none" height="36" viewBox="0 0 32 32" width="36">
+            <path
+                clipRule="evenodd"
+                d="M17.6482 10.1305L15.8785 7.02583L7.02979 22.5499H10.5278L17.6482 10.1305ZM19.8798 14.0457L18.11 17.1983L19.394 19.4511H16.8453L15.1056 22.5499H24.7272L19.8798 14.0457Z"
+                fill="currentColor"
+                fillRule="evenodd"
+            />
+        </svg>
+    );
+};
+
+const Header = async () => {
+    const session = await auth()
+    let authContent: React.ReactNode = null
+    if (session?.user) {
+        authContent =   <Avatar src={session.user.image || ''}/>
+    } else {
+        authContent = <NavbarItem className="flex gap-4">
+            <Button color="secondary"  variant="bordered">Sign in</Button>
+            <Button color="secondary" >Sign Up</Button>
+        </NavbarItem>
+    }
+
+    return (
+        <Navbar>
+            <NavbarBrand>
+                <AcmeLogo />
+                <p className="font-bold text-inherit">Discuss</p>
+            </NavbarBrand>
+            <NavbarContent className="hidden sm:flex gap-4" justify="center">
+                <NavbarItem>
+                    <Input></Input>
+                </NavbarItem>
+
+            </NavbarContent>
+            <NavbarContent justify="end">
+                {authContent}
+            </NavbarContent>
+        </Navbar>
+    );
+};
+
+export default Header;
